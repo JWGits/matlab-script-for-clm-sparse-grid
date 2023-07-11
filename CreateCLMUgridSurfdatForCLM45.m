92732a6961fd7b34cc97a0ad25e3c6ce46b739ca
Jon Wells <jonwells@hawaii.edu>
2023-07-10 19:30:58 -0700

    Update CreateCLMUgridSurfdatForCLM45.m

--- CreateCLMUgridSurfdatForCLM45.m
+++ CreateCLMUgridSurfdatForCLM45.m
@@ -94,10 +94,9 @@ if (time_found == 1)
 end
 
 in_dim_id = num2cell(in_dim_id)
-out_dim_id = 1:numel(out_dim_name)
-out_dim_id = num2cell(out_dim_id)
+out_dim_id = [1:numel(out_dim_name)]'
 in_dict = containers.Map(in_dim_id, in_dim_name)
-out_dict = containers.Map(out_dim_name, out_dim_id)
+out_dict = containers.Map(out_dim_name, num2cell(out_dim_id))
 
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %
